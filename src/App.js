ef04a8e5036390089189e6d4908dfd2779619899
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-04-14 21:52:34 +0530

    Added Search Filter

--- src/App.js
+++ src/App.js
@@ -46,6 +46,22 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header>">
+          <input
+            className="search-box"
+            type="search"
+            placeholder="search monsters"
+            onChange={(event) => {
+              console.log(event.target.value);
+              const searchString = event.target.value.toLocaleLowerCase();
+              const filteredMonsters = this.state.monsters.filter((monster) => {
+                return monster.name.toLocaleLowerCase().includes(searchString);
+              });
+
+              this.setState(() => {
+                return { monsters: filteredMonsters };
+              });
+            }}
+          />
           {this.state.monsters.map((monster) => {
             return <h1 key={monster.id}> {monster.name}</h1>;
           })}
